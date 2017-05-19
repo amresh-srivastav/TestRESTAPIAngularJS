59b0a6bc0b4e1c40fd2f788b631d48367a2dac4b
asrivatsav <asrivatsav.srivastav@incedoinc.com>
2017-05-19 05:35:32 -0500

    Uncommented app configuration

--- Scripts/Module.js
+++ Scripts/Module.js
@@ -14,9 +14,9 @@ var app;
         //$httpProvider.defaults.headers.patch = {};
 
         //$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
-        //$httpProvider.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
-        //$httpProvider.defaults.headers.get['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS';
-        //$httpProvider.defaults.headers.get['Access-Control-Allow-Headers'] = 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token';
+        $httpProvider.defaults.headers.get['Access-Control-Allow-Origin'] = '*';
+        $httpProvider.defaults.headers.get['Access-Control-Allow-Methods'] = 'PUT, GET, POST, DELETE, OPTIONS';
+        $httpProvider.defaults.headers.get['Access-Control-Allow-Headers'] = 'accept, content-type, x-parse-application-id, x-parse-rest-api-key, x-parse-session-token';
         $httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         $httpProvider.defaults.headers.get['Pragma'] = 'no-cache';
     } ]);
